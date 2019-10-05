7077cc1739a5673a288410b58642c7ae7358bd5a
Your Name <you@example.com>
2019-10-05 14:31:15 +0800

    add .xmind

--- src/main/java/com/shengsiyuan/jvm/classloader/MyTest27.java
+++ src/main/java/com/shengsiyuan/jvm/classloader/MyTest27.java
@@ -2,7 +2,6 @@ package com.shengsiyuan.jvm.classloader;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class MyTest27 {
     public static void main(String[] args) throws Exception {
